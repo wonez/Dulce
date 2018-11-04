9a212de31631d0595a91ecb684dd503451fe320b
wonez <ffazlic@yahoo.com>
2018-11-04 10:42:49 +0100

    edit profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,17 +41,27 @@ const editUser = async (req, res) => {
     form.uploadDir = 'public/images'
     form.keepExtensions = true;
     
-    form.parse(req, (err, fields, files) => {
-        console.log(fields, files)
+    form.parse(req, async (err, fields, files) => {
+        try{
+            //hash password
+            const updated = {...fields}
+            if(files.avatarUrlFile){
+                const paths = files.avatarUrlFile.path.split('/');
+                paths[0] = 'http://localhost:8000/';
+                updated.avatarUrl = paths.join('/');
+            }
+            if(files.coverUrlFile){
+                const paths = files.coverUrlFile.path.split('/');
+                paths[0] = 'http://localhost:8000/';
+                updated.coverUrl = paths.join('/');
+            }
+            const user = await User.findOneAndUpdate({ _id: req.params.id },updated, {new: true})
+            res.status(200).json(user);
+        } catch(err){
+            console.log(err);
+            res.status(500).end(err.message)
+        }
     })
-    // try{
-    //     const user = await User.findOneAndUpdate({ _id: req.params.id },{
-    //         ...req.body
-    //     }, {new: true})
-    //     res.status(200).json(user);
-    // }catch(err){
-    //     res.status(500).end(err.message)
-    // }
 }
 const deleteUser = async (req, res) => {
     try{
