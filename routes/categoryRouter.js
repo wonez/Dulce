f1e2249a92cebabe142124597d922813383044fb
wonez <ffazlic@yahoo.com>
2019-01-01 20:40:25 +0100

    public images

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -4,7 +4,7 @@ const categoryController = require('../controllers/categoryController')
 router.get('/', categoryController.getCategories)
 router.get('/:uri', categoryController.getCategoryPosts)
 //CATEGORIES ARE PRECREATED
-// router.post('/', categoryController.createCategory);
+router.post('/', categoryController.createCategory);
 // router.delete('/:id', categoryController.deleteCategory);
 
 
