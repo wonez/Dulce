ae81a77b442b79d743e9bce68783a75f9b2a73ec
wonez <ffazlic@yahoo.com>
2019-01-01 16:30:41 +0100

    env prd

--- server.js
+++ server.js
@@ -30,14 +30,13 @@ app.use(require('./utlity/tokenChecker'));
 
 app.use('/api', require('./routes.js'));
 
-// if(process.env.NODE_ENV === 'production'){
-//   console.log('prod');
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/dist'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   })
-// }
+}
 
 const server = app.listen(port, () => console.log(`Dulce listening on port ${port}!`))
 
