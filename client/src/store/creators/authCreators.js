7cc528ca1cc7d245494d65a87589942472d61007
wonez <ffazlic@yahoo.com>
2019-01-31 23:06:36 +0100

    firebase files saving

--- client/src/store/creators/authCreators.js
+++ client/src/store/creators/authCreators.js
@@ -98,12 +98,7 @@ const deleteCookies = () => {
 
 const createWebSocket = () => {
     return dispatch => {
-        let HOST = '';
-        if(location.port == 8080){
-            HOST = 'ws://localhost:8000'
-        }else{
-            HOST = location.origin.replace(/^http/, 'ws')
-        }
+        const HOST = location.origin.replace(/^http/, 'ws')
         const socket = new WebSocket(HOST)
         document.socket = socket;
         dispatch(handleSocket(socket));
--- client/src/utility/axios.js
+++ client/src/utility/axios.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 import store from '../store/config';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: '/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
